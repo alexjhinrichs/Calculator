60d83aece907ebb6769b1a195a7c9fea1b4b055b
Alex Hinrichs <alexjhinrichs@gmail.com>
2016-01-23 00:31:00 -0700

    Fully Functional

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
+var val = document.getElementById('screen');
 
+addValue = function(input) {
+	val.value += input;
 
-	
-});
+	if(input === 'c') {
+		val.value = '';
+	}
+};
+
+backspace = function() {
+	var number = val.value;
+	var newNumber = number.substring(0, number.length - 1);
+	val.value = newNumber;
+};
+
+answer = function() {
+	var expression = eval(val.value);
+	val.value = expression;
+};
